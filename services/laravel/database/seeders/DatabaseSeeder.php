e5bc2b1f44f512ae8654ef15986c81182b324c0a
ccccc <46977899+Xleone1@users.noreply.github.com>
2026-05-29 06:17:34 -0400

    Update DatabaseSeeder.php

--- services/laravel/database/seeders/DatabaseSeeder.php
+++ services/laravel/database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ItemSeeder::class,
             UserSeeder::class,
+            ItemSeeder::class,
             RoomSeeder::class,
         ]);
     }
